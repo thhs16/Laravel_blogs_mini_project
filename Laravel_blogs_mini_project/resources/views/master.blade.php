5902446cb5ffc571c6b10310e32ae037b5d3f399
thhs16 <thaehtapehtarsan@gmail.com>
2024-09-22 16:31:42 +0630

    Laravel_blogs_mini_project

---
+++ Laravel_blogs_mini_project/resources/views/master.blade.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Document</title>
+    {{-- Bootstrap CDN link CSS --}}
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
+</head>
+<body>
+
+</body>
+    {{-- Bootstrap CDN link JS --}}
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz" crossorigin="anonymous"></script>
+    {{-- Fontawesome Kit JS inclusive --}}
+    <script src="https://kit.fontawesome.com/905758c01a.js" crossorigin="anonymous"></script>
+</html>
